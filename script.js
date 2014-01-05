fe855483d9ebe3afd73bc20a8960fe49e39a0ef9
Silverballs <asilbersmith11@gmail.com>
2014-01-04 21:31:34 -0500

    Add second contact, created an array for contacts, and tested to make sure I get the correct info from accessing an index of array

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ var andrew = {
 };
 // make sure object is set up good by inspecting page
 console.log(andrew);
+// to access a property of andrew you would do console.log(andrew.key)
+
+// add another contact:
+
+var myMother = {
+	firstName: "Jayne",
+	lastName: "Dellon",
+	phoneNumber: "123-245-7890",
+	email: "mymother@yourmother.com"
+};
+
+// create an array to store your contacts in... 
+// var contacts [object1, object2]
+var contacts = [andrew, myMother]
+
+// making sure you can access your contacts do:
+console.log(contacts[1]) // this will access myMother through the array;
+
