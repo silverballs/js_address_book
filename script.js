61663044ac487fab8c5c9c217e28fb355016550d
Silverballs <asilbersmith11@gmail.com>
2014-01-04 21:14:27 -0500

    Add object and tested in browser

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.prompt('test');
+// window.prompt('test');
+
+
+// create an object 
+
+var andrew = {
+	firstName: "Andrew",
+	lastName: "Silbersmith",
+	phoneNumber: "973-818-6390",
+	email: "asilbersmith11@gmail.com"
+};
+// make sure object is set up good by inspecting page
+console.log(andrew);
